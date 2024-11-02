e00f500cbde8a8725fb3acd42e7231f271a91c22
sandraks8891 <sandraks8891@gmail.com>
2024-11-02 15:58:59 +0530

    nov 02

---
+++ first.py
@@ -0,0 +1,37 @@
+# print('''Hello 
+#       World
+#       welcome''')
+# a=10
+# print(a)
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# c=a+b
+# print(c)
+
+# a=(input("enter a number"))
+# a=int(a)
+# b=(input("enter a number"))
+# b=int(b)
+# c=a*b
+# print(c)
+
+# a=(input("enter a number"))
+# a=int(a)
+# b=(input("enter a number"))
+# b=int(b)
+# c=a/b
+# print(c)
+
+# a=(input("enter a number"))
+# a=int(a)
+# b=(input("enter a number"))
+# b=int(b)
+# c=a%b
+# print(c)
+
+# a=(input("enter a number"))
+# a=int(a)
+# b=(input("enter a number"))
+# b=int(b)
+# c=a//b
+# print(c)
---
+++ second.py
@@ -0,0 +1,51 @@
+# swapping
+# a=5
+# b=10
+# a=a+b
+# b=a-b
+# a=a-b
+# print(a)
+# print(b)
+
+# a=10
+# b=20
+# a,b=b,a
+# print(a)
+# print(b)
+
+# a=10
+# a=20
+# print(a)
+
+# datatypes
+# a=10
+# print(type(a))
+# a=10.2
+# print(type(a))
+# a=10+5j
+# print(type(a))
+# a='hi'
+# print(type(a))
+# a=['hello']
+# print(type(a))
+# a=('hello','hi')
+# print(type(a))
+# a={'hello'}
+# print(type(a))
+# a={'Name':'sandra'}
+# print(type(a))
+# a=True
+# b=False
+# print(type(a),type(b))
+
+
+# a=20
+# b=40
+# c=a+b
+# print(c)
+# print(A)
+# print(b)
+
+# casing
+# myFirstVariable=10
+# print(Myfirstvariable)
