8d5bd4e4f8d5159656e8e601e722c845993afe0c
sandraks8891 <sandraks8891@gmail.com>
2024-11-05 16:23:01 +0530

    nov 05

---
+++ 4th.py
@@ -0,0 +1,98 @@
+#  string methods
+# a="synnefo"
+# b=a.capitalize()
+# print(b)
+
+
+# a="Synnefo is in kochi"
+# b=a.count("is")
+# print(b)
+
+
+# a="Synnefoisin9800Kochi"
+# b=a.isalnum()
+# print(b)
+
+
+# a="1234567rrtt"
+# b=a.isdigit()
+# print(b)
+
+# a="Synnefoisinkochi"
+# b=a.isalpha()
+# print(b)
+
+# a="Synnefoisinkochi"
+# b=a.isalpha()
+# print(b)
+
+# a="synnefoisinkochi"
+# b=a.islower()
+# print(b)
+
+# a="Synnefoisinkochi"
+# b=a.isalpha()
+# print(b)
+
+# a="Synnefoisinkochi"
+# b=a.split("n")
+# print(b)
+
+
+
+# convert to lowercase
+# a=(input("enter a word:"))
+# b=a.lower()
+# print(b)
+
+# convert to uppercase
+# a=(input("enter a word:"))
+# b=a.upper()
+# print(b)
+
+# capitalize the first letter
+# a=(input("enter a word:"))
+# b=a.capitalize()
+# print(b)
+
+# count 
+# a=(input("enter a word:"))
+# b=(input('enter a letter:'))
+# c=a.count(b)
+# print(c)
+
+
+# startswith
+# a=(input("enter a word:"))
+# b=(input("enter a letter:"))
+# c=a.startswith(b)
+# print(c)
+
+# endswith
+# a=(input("enter a word:"))
+# b=(input("enter a letter:"))
+# c=a.endswith(b)
+# print(c)
+
+
+# remove whitespace
+# a=(input("enter a word:"))
+# c=a.strip()
+# print(c)
+
+# replace a word
+# a=(input("enter a string:"))
+# b=(input("enter a word to replace:"))
+# d=(input("enter a word:"))
+# c=a.replace(b,d)
+# print(c)
+
+# split a string into words
+# a=(input("enter a word:"))
+# # b=(input("enter a split:"))
+# c=a.split()
+# print(c)
+
+
+# join words
+# a=(input("enter a word1:"))
