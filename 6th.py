a3c19009c664b3d80e55afd3eac85383737d7b88
sandraks8891 <sandraks8891@gmail.com>
2024-11-08 15:27:03 +0530

    nov08

---
+++ 6th.py
@@ -0,0 +1,33 @@
+# while loop
+# i=0
+# while i<5:
+#     print(i)
+#     i+=1
+# else:
+#     print("greater than")
+
+
+# reverse a number using while loop
+# a=int(input("enter a number"))
+# b=0
+# while a>0:
+#     c=a%10
+#     b=(b*10)+c
+#     a=a//10
+# print(b)
+
+# palindrome
+a=int(input("enter a number"))
+d=a
+b=0
+while a>0:
+    c=a%10
+    b=(b*10)+c
+    a=a//10
+if b==d:
+    print("palindrome")
+else:
+    print("not a palindrome")
+    
+    
+    
