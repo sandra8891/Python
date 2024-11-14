98755e2a0220dc032a25735f8687d3f8b32cb758
sandraks8891 <sandraks8891@gmail.com>
2024-11-14 15:47:19 +0530

    nov14

---
+++ for.py
@@ -0,0 +1,23 @@
+# fibonacci sequence
+# a=0
+# b=1
+# c=int(input("enter a number"))
+# print(a)
+# print(b)
+# for i in range(c):
+#     d=a+b
+#     a=b
+#     b=d
+#     print(d)
+# print()
+
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# c=int(input("enter a limit"))
+# print(a)
+# print(b)
+# for i in range(c):
+#     d=a+b
+#     a=b
+#     b=d
+#     print(d)
---
+++ list.py
@@ -0,0 +1,35 @@
+# list
+# a=[1,"apple",[1,2,3],1]
+# for i in a:
+#     print(i)
+
+# b=[]
+# for i in b:
+#     print(i)
+
+
+# list length
+# a=[1,2,"apple","orange"]
+# b=len(a)
+# print(b)
+
+# a="orange"
+# print(a[-1])
+
+# a="orange"
+# print(len(a))
+
+# a=[1,2,"apple","orange"]
+# print(a[1:3])
+
+# a=[1,2,"apple","orange"]
+# a[1]="grape"
+# print(a)
+
+# a=[1,2,"apple","orange"]
+# a[1:3]=["cherry","pineapple"]
+# print(a)
+
+a=[1,2,"apple","orange"]
+a[1:3]=["cherry","pineapple","jackfruit"]
+print(a)
