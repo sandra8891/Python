93ffe2a1b5b4f4d12395854130086666542ac700
sandraks8891 <sandraks8891@gmail.com>
2024-11-30 15:12:45 +0530

    nov30

--- data_abstraction.py
+++ data_abstraction.py
@@ -1,10 +1,10 @@
-from abc import ABC,abstractmethod
-class Master(ABC):
-    @abstractmethod
-    def master1():
-        pass
-class Main(Master):
-    def master1():
-        print("hiiiii")
-obj1=Main
-obj1.master1()
+# from abc import ABC,abstractmethod
+# class Master(ABC):
+#     @abstractmethod
+#     def master1():
+#         pass
+# class Main(Master):
+#     def master1():
+#         print("hiiiii")
+# obj1=Main
+# obj1.master1()
