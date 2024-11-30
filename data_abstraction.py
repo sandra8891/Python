85ac1e080d84ba3d42a7510fb911aeef3b372461
sandraks8891 <sandraks8891@gmail.com>
2024-11-30 15:10:20 +0530

    nov29

---
+++ data_abstraction.py
@@ -0,0 +1,10 @@
+from abc import ABC,abstractmethod
+class Master(ABC):
+    @abstractmethod
+    def master1():
+        pass
+class Main(Master):
+    def master1():
+        print("hiiiii")
+obj1=Main
+obj1.master1()
